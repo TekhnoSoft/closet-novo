42a0548dd50b212e3fdbabe76534b76100005809
Marcos Paulo <tekhnosoftdevelop@gmail.com>
2024-11-04 14:02:38 -0300

    Update users.js

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -128,4 +128,12 @@ router.post('/register', validateOrigin, async (req, res) => {
     }
 })
 
+router.post('/convert-to-admin', validateToken, async (req, res) => {
+    try{
+        return res.status(200).json({ success: true, code: 200, message: "Convertido!" });
+    }catch (error) {
+        return res.status(400).json({ success: false, code: 400, message: error.message });
+    }
+})
+
 module.exports = router;
