b87d57afc4987d20583a77f809b4963fec20ac85
Marcos Paulo <tekhnosoftdevelop@gmail.com>
2024-11-13 20:16:59 -0300

    Update Environment.js

--- frontend/src/Environment.js
+++ frontend/src/Environment.js
@@ -1,7 +1,7 @@
-const DEVELOPMENT_MODE = false;
+const DEVELOPMENT_MODE = true;
 
 const Environment = {
-    API_BASE: (DEVELOPMENT_MODE) ? "http://localhost:3001" : "https://closet-novo-backend.7richv.easypanel.host/",
+    API_BASE: (DEVELOPMENT_MODE) ? "http://localhost:3001" : "https://closet-novo-backend.7richv.easypanel.host",
     HEADERS_CLIENTE: { 
         headers: { 
             CLOSETNOVO_CLIENTE_ACCESS_TOKEN : localStorage.getItem("closetnovo_cliente_token")
